7b9816c67a6746da1f6ed525d3045a595d13c3e5
Pykinho <miko.pyka@gmail.com>
2023-12-12 17:27:36 +0100

    Project setup

---
+++ printers_manager/electron/electron.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let win: Electron.BrowserWindow | null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
